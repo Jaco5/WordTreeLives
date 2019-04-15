e7119b9edb5d8ea48a8090c44781e3bfc442089b
Jake <jacobartz1987@gmail.com>
2019-04-15 10:26:03 -0600

    App.js - initial

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
+import UserInput from "./UserInput/UserInput";
+import DisplayArticle from "./DisplayArticle/DisplayArticle";
+import DisplayTree from "./DisplayTree/DisplayTree";
 
 import './App.css';
 
 class App extends Component {
+
+  state = {
+
+  }
+
+  handleInputChange = (event) => {
+
+  }
+
+  createTreeData = () => {
+
+  }
+
+  findThatArticle = () => {
+    
+  }
+
+
+
+
   render() {
     return (
       <div className="App">
-        
+        <h1>I am groot!</h1>
+        <UserInput />
+        <UserInput />
+        <UserInput />
+        <DisplayArticle />
+        <DisplayTree />
+
         
       </div>
     );
